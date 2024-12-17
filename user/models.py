08ac3e1184517fdeb63063e692b83c980a094a0c
Ray Carter <raycarter158@gmail.com>
2024-12-17 11:34:20 +0000

    feat: create and register Profile model

--- user/models.py
+++ user/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = CloudinaryField('image', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
 
