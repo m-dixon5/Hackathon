4870b02b10c99fbec8f03f2a600a281078551790
Ray Carter <raycarter158@gmail.com>
2024-12-17 15:34:52 +0000

    fix: home urls.py apps.py, and blog models.py configuration

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django_ckeditor_5.fields import CKEditor5Field 
 
 # Create your models here.
 
--- home/urls.py
+++ home/urls.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
-from home.views import index
+from .views import HomePage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('profile/', include('user.urls')),
-    path("", views.HomePage.as_view(), name="home"),
-
+    path("",HomePage.as_view(), name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
