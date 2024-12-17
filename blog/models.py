63345dd8a7060517081a7fea3f9373017fb29104
Ray Carter <raycarter158@gmail.com>
2024-12-17 14:37:49 +0000

    feat: create category model and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Category(models.Model):
+    """
+    Represents a music genre category for live events
+    """
+    GENRE_CHOICES = [
+        ('rock', 'Rock'),
+        ('pop', 'Pop'),
+        ('hip-hop', 'Hip-Hop'),
+        ('jazz', 'Jazz'),
+        ('classical', 'Classical'),
+        ('electronic', 'Electronic'),
+        ('blues', 'Blues'),
+        ('metal', 'Metal'),
+        ('reggae', 'Reggae'),
+        ('folk', 'Folk'),
+        ('punk', 'Punk'),
+        ('indie', 'Indie'),
+        ('rnb', 'R&B'),
+        ('country', 'Country'),
+        ('soul', 'Soul'),
+        ('funk', 'Funk'),
+        ('latin', 'Latin'),
+        ('world', 'World Music'),
+        ('house', 'House'),
+        ('techno', 'Techno'),
+    ]
+    title = models.CharField(max_length=30, choices=GENRE_CHOICES, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        """
+        Auto-generate slug from title if not provided
+        """
+        if not self.slug:
+            self.slug = slugify(self.get_title_display())
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.get_title_display()
+
+
