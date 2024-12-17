54f21fc621823fb5eb6ebc9f02165106491c326c
Ray Carter <raycarter158@gmail.com>
2024-12-17 12:18:57 +0000

    chore: profile views

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.core.paginator import Paginator
 
 # Create your views here.
 
