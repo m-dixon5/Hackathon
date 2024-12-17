b4907c55050bfcce5a612c60c10f7a9dbc9a1b6c
Ray Carter <raycarter158@gmail.com>
2024-12-17 15:11:06 +0000

    chore: update home urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
-from . import views
-from django.urls import path
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
+from home.views import index
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('profile/', include('user.urls')),
     path("", views.HomePage.as_view(), name="home"),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
