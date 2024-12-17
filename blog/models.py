7d3c88f1eece1785b981659ebdbc973a014af5dd
Ray Carter <raycarter158@gmail.com>
2024-12-17 14:45:56 +0000

    feat: create review model and apply migrations

--- blog/models.py
+++ blog/models.py
@@ -39,5 +39,29 @@ class Category(models.Model):
     def get_absolute_url(self):
         return f"/{self.slug}"
 
+class Review(models.Model):
+    """
+    Represents music blog posts
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.FileField(upload_to='media/blog')
+    slug = models.SlugField(default='')
+    content = CKEditor5Field('Content', config_name='extends')
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    location = models.CharField(max_length=30)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return '/%s/' % self.slug
+
 
 
